beee4dba819890f5598f79b94b209dd749d829f1
Alok Chaudhary <34033578+Beginneronhunt@users.noreply.github.com>
2023-09-14 13:09:09 +0530

    Update BasePSScript.ps1

--- BasePSScript.ps1
+++ BasePSScript.ps1
@@ -33,8 +33,8 @@ Add-Content -Path $logpath -Value "$timestamp Device Restart"
 #Autologin to Windows Account
 # Set the registry values for auto-login
 $regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$defaultUsername = "Rajkot 42G"
-$defaultPassword = "Test@1234"
+$defaultUsername = ""
+$defaultPassword = ""
 Set-ItemProperty -Path $regPath -Name "AutoAdminLogon" -Value "1"
 Set-ItemProperty -Path $regPath -Name "DefaultUsername" -Value $defaultUsername
 Set-ItemProperty -Path $regPath -Name "DefaultPassword" -Value $defaultPassword
